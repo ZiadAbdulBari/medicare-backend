b539bd37dbcb4dbe4b4e015219f85efed8acd433
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2022-09-07 11:07:35 +0600

    page added

--- controllers/admin.comtroller.js
+++ controllers/admin.comtroller.js
@@ -15,5 +15,25 @@ router.get('/doctor-list', async (req,res)=>{
         title: 'Doctor List',
     });
 })
+router.get('/core-service', async (req,res)=>{
+    res.render('pages/core-service', {
+        title: 'Core Service',
+    });
+})
+router.get('/medical-service', async (req,res)=>{
+    res.render('pages/medical-service', {
+        title: 'Medical Service',
+    });
+})
+router.get('/create-appointment', async (req,res)=>{
+    res.render('pages/create-appointment', {
+        title: 'Create Appointment',
+    });
+})
+router.get('/all-appointment', async (req,res)=>{
+    res.render('pages/all-appointment', {
+        title: 'All Appointment',
+    });
+})
 
 module.exports=router;
